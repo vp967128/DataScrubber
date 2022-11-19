883610d04d218d557bdcb02322da086514aa2a7b
vp967128 <80238419+vp967128@users.noreply.github.com>
2022-11-18 17:05:59 -0800

    Add files via upload
    latest working file

--- App.js
+++ App.js
@@ -1,9 +1,72 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import React, { useEffect } from 'react';
+// import React, { useState } from 'react';
+import { Space, Table, Tag } from 'antd';
 import axios from 'axios';
 import './App.css';
 
 function App() {
+  const { Column, ColumnGroup } = Table;
+  const data = [
+    {
+      key: '1',
+      firstName: 'John',
+      lastName: 'Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      firstName: 'Jim',
+      lastName: 'Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      firstName: 'Joe',
+      lastName: 'Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
+  // const dataSource = [
+  //   {
+  //     key: '1',
+  //     name: 'Mike',
+  //     age: 32,
+  //     address: '10 Downing Street',
+  //   },
+  //   {
+  //     key: '2',
+  //     name: 'John',
+  //     age: 42,
+  //     address: '10 Downing Street',
+  //   },
+  // ];
+  
+  // const columns = [
+  //   {
+  //     title: 'Name',
+  //     dataIndex: 'name',
+  //     key: 'name',
+  //   },
+  //   {
+  //     title: 'Age',
+  //     dataIndex: 'age',
+  //     key: 'age',
+  //   },
+  //   {
+  //     title: 'Address',
+  //     dataIndex: 'address',
+  //     key: 'address',
+  //   },
+  // ];
+  
   useEffect(() => {
     console.log('I am here');
     axios.get('http://127.0.0.1:5000/files')
@@ -11,24 +74,71 @@ function App() {
         console.log(res.data);
       })
   }, []);
+
+  const App = () => (
+    <Table dataSource={data}>
+      <ColumnGroup title="Name">
+        <Column title="First Name" dataIndex="firstName" key="firstName" />
+        <Column title="Last Name" dataIndex="lastName" key="lastName" />
+      </ColumnGroup>
+      <Column title="Age" dataIndex="age" key="age" />
+      <Column title="Address" dataIndex="address" key="address" />
+      <Column
+        title="Tags"
+        dataIndex="tags"
+        key="tags"
+        render={(tags) => (
+          <>
+            {tags.map((tag) => (
+              <Tag color="blue" key={tag}>
+                {tag}
+              </Tag>
+            ))}
+          </>
+        )}
+      />
+      <Column
+        title="Action"
+        key="action"
+        render={(_, record) => (
+          <Space size="middle">
+            <a>Invite {record.lastName}</a>
+            <a>Delete</a>
+          </Space>
+        )}
+      />
+    </Table>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Table dataSource={data}/>;
     </div>
+
+
+    // <div>
+    //   <Table dataSource={dataSource} columns={columns} />;
+    // </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
+
 
 export default App;
